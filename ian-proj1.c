cde146b42ffae4ed3cc58fcf7070a6ce98fbd386
0x21q <xkrato67@stud.fit.vutbr.cz>
2023-02-21 22:07:13 +0100

    adding sections and header file

--- ian-proj1.c
+++ ian-proj1.c
@@ -1,77 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <libelf.h>
-#include <gelf.h>
-#include <fcntl.h>
-#include <unistd.h> 
-#include <string.h>
-
-int parse_args(int argc, char **argv);
-char * get_type(Elf64_Word type);
-
-int parse_args(int argc, char **argv) {
-    if (argc != 2) {
-        puts("Usage: ./ian-proj1 <ELF binary>");
-        exit(1);
-    }
-    int fd;
-    if ((fd = open(argv[1], O_RDONLY, 0)) < 0) {
-        puts("Error opening file");
-        exit(1);
-    }
-    return fd;
-}
-
-char * get_type(Elf64_Word type) {
-    switch (type) {
-        case 0:
-            return "NULL";
-        case 1:
-            return "LOAD";
-        case 2:
-            return "DYNAMIC";
-        case 3:
-            return "INTERP";
-        case 4:
-            return "NOTE";
-        case 5:
-            return "SHLIB";
-        case 6:
-            return "PHDR";
-        case 7:
-            return "TLS";
-        default:
-            return "-";
-    }
-}
-
-char * get_perm(Elf64_Word perm) {
-    switch(perm) {
-        case 0:
-            return "none";
-        case 1:
-            return "--X";
-        case 2:
-            return "-W-";
-        case 3:
-            return "-WX";
-        case 4:
-            return "R--";
-        case 5:
-            return "R-X";
-        case 6:
-            return "RW-";
-        case 7:
-            return "RWX";
-        default:
-            return "-";
-    }
-}
+#include "ian-proj1.h"
 
 int main(int argc, char **argv) {
-    Elf * elf;
-    int arch;
-    size_t n;
 
     int fd = parse_args(argc, argv);
 
@@ -80,6 +9,8 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    Elf * elf;
+    // Getting the elf file
     if ((elf = elf_begin(fd, ELF_C_READ, NULL)) == NULL) {
         puts("Error opening elf file");
         return 1;
@@ -89,32 +20,69 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    int arch;
+    // Getting the architecture of the elf file
     if ((arch = gelf_getclass(elf)) == ELFCLASSNONE) {
-        puts("Error getting architecture");
+        puts("Error unknown architecture");
         return 1;
     }
 
     printf("%s : %d - bit ELF object\n", argv[1],arch == ELFCLASS32 ? 32 : 64);
 
+    size_t n;
+    // Getting the number of segments
     if (elf_getphdrnum(elf, &n) != 0) {
         puts("Error while getting number of segments");
         return 1;
     }
 
     GElf_Phdr phdr;
+    char * type, * perm;
 
-    printf("%-8s %-15s %-5s %-s\n", "Segment", "Type", "Perm", "Sections");
+    // Printing the segments and their information
+    printf("%-7s %-12s %-10s %-4s %-s\n", "Segment", "Type", "Raw type", "Perm", "Sections");
 
-    for(size_t i = 0; i < n; ++i) {
+    for (size_t i = 0; i < n; ++i) {
         if (gelf_getphdr(elf, i, &phdr) != &phdr) {
             puts("Error getting program header table");
             return 1;
         }
-        char * type = !strcmp(get_type(phdr.p_type), "-") ? "-" : get_type(phdr.p_type);
-        char * perm = get_perm(phdr.p_flags);
-        printf("%-8ld %-15s %-5s %-s\n", i, type, perm, "sections");
+        type = get_type(phdr.p_type);
+        perm = get_perm(phdr.p_flags);
+        printf("%-7ld %-12s %-#10x %-4s %-s\n", i, type, phdr.p_type, perm, "todo");
     }
 
+    puts("");
+
+    size_t shstridx;
+    // Getting the section header string table index
+    if (elf_getshdrstrndx(elf, &shstridx) != 0) {
+        puts("Error getting section header string table index");
+        return 1;
+    }
+
+    Elf_Scn * section = NULL;
+    GElf_Shdr shdr;
+    char * name;
+
+    printf("%-7s %s\n", "Section", "Name");
+
+    for (int i = 0; (section = elf_nextscn(elf, section)) != NULL; ++i) {
+        // Getting the section header
+        if (gelf_getshdr(section, &shdr) != &shdr) {
+            puts("Error getting section header");
+            return 1;
+        }
+        // Getting the section name
+        if ((name = elf_strptr(elf, shstridx, shdr.sh_name)) == NULL) {
+            puts("Error getting section name");
+            return 1;
+        }
+
+        printf("%-7d %s\n", (i+1), name);
+    }
+
+    elf_end(elf);
     close(fd);
     return 0;
 }
